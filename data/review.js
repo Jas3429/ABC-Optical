6974c54610704f869896a7dff63d8e578c12686b
Jas3429 <jason.a.sheriff@gmail.com>
2023-05-01 12:37:55 +1000

    first commit

---
+++ data/review.js
@@ -0,0 +1,16 @@
+const reviews = [
+  {
+    review:
+      "Squid blog vinyl meditation, aesthetic meh subway tile four loko lomo keytar master cleanse etsy church-key ramps portland. Yuccie blue bottle ugh affogato photo booth kogi. Vaporware squid messenger bag vinyl kitsch salvia schlitz gastropub cronut raclette plaid cloud bread asymmetrical cupping. Subway tile skateboard hell of you probably haven't heard of them copper mug tote bag squid af air plant mustache vinyl.",
+  },
+  {
+    review:
+      "Chartreuse shoreditch gentrify man braid keffiyeh. Fingerstache vinyl deep v pug bodega boys biodiesel raclette +1 kickstarter, hashtag same. Venmo pabst single-origin coffee hella shoreditch, DIY +1 truffaut farm-to-table flannel kitsch bicycle rights. Cold-pressed ramps marxism, tumeric fingerstache williamsburg normcore kickstarter neutral milk hotel. Street art truffaut viral skateboard tattooed. Ascot shoreditch gentrify franzen paleo solarpunk keytar blue bottle roof party iceland sriracha chambray offal bitters slow-carb.",
+  },
+  {
+    review:
+      "Palo santo gentrify celiac, post-ironic biodiesel food truck letterpress big mood tote bag pitchfork XOXO af deep v sriracha. Lyft next level hexagon master cleanse tonx flexitarian YOLO activated charcoal DSA craft beer semiotics taxidermy mukbang gastropub. Kale chips praxis narwhal skateboard. Gochujang master cleanse mlkshk cloud bread bushwick kombucha ethical knausgaard schlitz.",
+  },
+];
+
+export default reviews;
